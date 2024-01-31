35050797f460f9311f9c0decf0a4be7414b8f500
Shubham Bahre <shubham.bahre@viklele.com>
2024-01-31 22:07:54 +0530

    Added add row and remove row functionality

--- src/app/pages/inline-editable-table/inline-editable-table.component.ts
+++ src/app/pages/inline-editable-table/inline-editable-table.component.ts
@@ -1,15 +1,43 @@
 import { Component } from '@angular/core';
 
 const USER_DATA = [
-  { name: 'John Smith', occupation: 'Advisor', age: 36 },
-  { name: 'Muhi Masri', occupation: 'Developer', age: 28 },
-  { name: 'Peter Adams', occupation: 'HR', age: 20 },
-  { name: 'Lora Bay', occupation: 'Marketing', age: 43 },
+  {
+    id: 1,
+    name: 'John Smith',
+    occupation: 'Advisor',
+    dateOfBirth: '1984-05-05',
+    age: 36,
+  },
+  {
+    id: 2,
+    name: 'Muhi Masri',
+    occupation: 'Developer',
+    dateOfBirth: '1992-02-02',
+    age: 28,
+  },
+  {
+    id: 3,
+    name: 'Peter Adams',
+    occupation: 'HR',
+    dateOfBirth: '2000-01-01',
+    age: 20,
+  },
+  {
+    id: 4,
+    name: 'Lora Bay',
+    occupation: 'Marketing',
+    dateOfBirth: '1977-03-03',
+    age: 43,
+  },
 ];
-
+/**
+ *  key: name of the property which provides the data for the column
+    type: datatype of the columns
+    label: Header row text for the column,
+ */
 const COLUMNS_SCHEMA = [
   {
-    key: 'name',
+    key: 'name', //
     type: 'text',
     label: 'Full Name',
   },
@@ -17,6 +45,11 @@ const COLUMNS_SCHEMA = [
     key: 'occupation',
     type: 'text',
     label: 'Occupation',
+  },
+  {
+    key: 'dateOfBirth',
+    type: 'date',
+    label: 'Date of Birth',
   },
   {
     key: 'age',
@@ -41,4 +74,20 @@ export class InlineEditableTableComponent {
   displayedColumns: string[] = COLUMNS_SCHEMA.map((col) => col.key);
   dataSource: any = USER_DATA;
   columnsSchema: any = COLUMNS_SCHEMA;
+
+  addRow() {
+    const newRow = {
+      id: Date.now(),
+      name: '',
+      occupation: '',
+      dateOfBirth: '',
+      age: 0,
+      isEdit: true,
+    };
+    this.dataSource = [newRow, ...this.dataSource];
+  }
+
+  removeRow(id: number) {
+    this.dataSource = this.dataSource.filter((u: any) => u.id !== id);
+  }
 }
