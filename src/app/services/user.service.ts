af679bd7f4c10b4b303b03fab319b57765f14f64
Shubham Bahre <shubham.bahre@viklele.com>
2024-02-02 22:06:22 +0530

    Added promise calls in service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, forkJoin } from 'rxjs';
+import { Observable, forkJoin, lastValueFrom } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { User } from '../pages/api-editable-table/api-editable-table.component';
 
@@ -18,19 +18,48 @@ export class UserService {
       .pipe<User[]>(map((data: any) => data.users));
   }
 
+  getUsersAsync(): Promise<User[]> {
+    let result$ = this.http
+      .get(this.serviceUrl)
+      .pipe<User[]>(map((data: any) => data.users));
+    return lastValueFrom(result$);
+  }
+
   updateUser(user: User): Observable<User> {
     return this.http.patch<User>(`${this.serviceUrl}/${user.id}`, user);
+  }
+
+  updateUserAsync(user: User): Promise<User> {
+    return lastValueFrom(
+      this.http.patch<User>(`${this.serviceUrl}/${user.id}`, user)
+    );
   }
 
   addUser(user: User): Observable<User> {
     return this.http.post<User>(`${this.serviceUrl}/add`, user);
   }
 
+  addUserAsync(user: User): Promise<User> {
+    return lastValueFrom(this.http.post<User>(`${this.serviceUrl}/add`, user));
+  }
+
   deleteUser(id: number): Observable<User> {
     return this.http.delete<User>(`${this.serviceUrl}/${id}`);
   }
 
+  deleteUserAsync(id: number): Promise<User> {
+    return lastValueFrom(this.http.delete<User>(`${this.serviceUrl}/${id}`));
+  }
+
   deleteUsers(users: User[]): Observable<User[]> {
+    return forkJoin(
+      users.map((user) =>
+        this.http.delete<User>(`${this.serviceUrl}/${user.id}`)
+      )
+    );
+  }
+
+  deleteUsersAsync(users: User[]): Observable<User[]> {
     return forkJoin(
       users.map((user) =>
         this.http.delete<User>(`${this.serviceUrl}/${user.id}`)
